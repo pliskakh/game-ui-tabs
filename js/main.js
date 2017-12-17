af19030662858cf934e957ac9ac0dd4698654e7f
Mykhailo Pliskunov <pliskunov.mykhailo@gmail.com>
2017-12-17 12:53:57 +0200

    logic added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-;(function(){
+;
+(function () {
 	"use strict";
-	
+
+	$(document).ready(function () {
+
+		$('.ba-tab-menu').on('click', 'a', function (e) {
+			e.preventDefault();
+
+			$('.ba-tab-menu__item').removeClass('ba-tab-menu__item--active');
+			$(this).addClass('ba-tab-menu__item--active');
+
+			var tabId = $(this).data('tab');
+			$('.ba-tab').removeClass('ba-tab--active');
+			$('#' + tabId).addClass('ba-tab--active');
+
+		});
+
+	});
+
 })();
